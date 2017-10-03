7f7ffca36e3a2e8588eded42453e2f483fc95069
Mads Mosumgaard <mads1108@hotmail.dk>
2017-10-03 15:47:23 +0200

    wdwa

--- BlackJackGame/BlackJackGame/CardDeck.cs
+++ BlackJackGame/BlackJackGame/CardDeck.cs
@@ -1,25 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 
-/// <summary>
-/// Summary description for Class1
-/// </summary>
+
 public class CardDeck
 {
+    private List<Card> deck;
+
 	public CardDeck()
 	{
-	    List<Card> cardDeck = new List<Card>();
-        for (int i = 0; i < 13; i++)
-	    {
-	        for (int j = 0; j < 4; j++)
-	        {
-	           Card card = new Card(j, i+1);
-	            cardDeck.Add(card);
-	        }
-	    }
+	    deck = new List<Card>();
         
-	   
-
 	}
+
+private void FillDeck()
+{
+    for (int i = 0; i < 13; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            Card card = new Card(j, i+1);
+            cardDeck.Add(card);
+        }
+    }
+}
+
+private void Shuffle(List deck)
+{
+    
+}
     
 }
